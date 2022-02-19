f8ad86fc195616b7f0496ff97bbab8840a951114
Young Zoo Park <enkeejuniour@naver.com>
2022-02-20 00:07:06 +0900

    당연한 얘기지만..

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log(523432);
+console.log("String");
+console.log('String');
+console.log(5+2);
+console.log(5*2);
+console.log(5235/2432);
+
+const a = 5;
+const b = "a";
+console.log(a*10);
+console.log(a-10);
+console.log(b);
+

63cc258bd66c6b1106d09e5af4024cb770f95699
Young Zoo Park <enkeejuniour@naver.com>
2022-02-20 12:37:35 +0900

    update app.js

--- app.js
+++ app.js
@@ -1,24 +1,4 @@
-const calculator = {
-    add:function addition(fNum, sNum){
-        return (fNum+sNum);
-    },
-    subtract:function minus(fNum, sNum){
-        return (fNum-sNum);
-    },
-    multiply:function multi(fNum, sNum){
-        return (fNum*sNum);
-    },
-    divide:function divid(fNum, sNum){
-        return (fNum/sNum);
-    },
-    power:function pow(fNum, sNum){
-        return (fNum**sNum);
-    }
-}
-
-const plusResult = calculator.add(1,2);
-const minusResult = calculator.subtract(1,2);
-const timesResult = calculator.multiply(1,2);
-const divideResult = calculator.divide(timesResult,2);
-const powerResult = calculator.power(1,divideResult);
-
+const title = document.getElementById("title");
+title.innerText="got you";
+console.log(title.id);
+console.log(title.innerText);

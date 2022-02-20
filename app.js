3857acc3d0b54bafdb36946df1dc4d86fa3ef4ce
Young Zoo Park <enkeejuniour@naver.com>
2022-02-20 12:00:53 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-const player = {
-    name:"nico",
-    sayHello:function(name){
-        console.log("Hello "+name+" nice to meet you");
+const calculator = {
+    add:function addition(fNum, sNum){
+        return (fNum+sNum);
     },
-};
-console.log(player.name);
-player.sayHello("nico");
+    subtract:function minus(fNum, sNum){
+        return (fNum-sNum);
+    },
+    multiply:function multi(fNum, sNum){
+        return (fNum*sNum);
+    },
+    divide:function divid(fNum, sNum){
+        return (fNum/sNum);
+    },
+    power:function pow(fNum, sNum){
+        return (fNum**sNum);
+    }
+}
+
+const plusResult = calculator.add(1,2);
+const minusResult = calculator.subtract(1,2);
+const timesResult = calculator.multiply(1,2);
+const divideResult = calculator.divide(timesResult,2);
+const powerResult = calculator.power(1,divideResult);
+

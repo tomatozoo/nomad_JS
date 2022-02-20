831aa0063137cbda24172f540866efcf268532fa
Young Zoo Park <enkeejuniour@naver.com>
2022-02-20 11:25:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-console.log(523432);
-console.log("String");
-console.log('String');
-console.log(5+2);
-console.log(5*2);
-console.log(5235/2432);
-
-const a = 5;
-const b = "a";
-console.log(a*10);
-console.log(a-10);
-console.log(b);
-
+const player = {
+    name:"nico",
+    sayHello:function(name){
+        console.log("Hello "+name+" nice to meet you");
+    },
+};
+console.log(player.name);
+player.sayHello("nico");

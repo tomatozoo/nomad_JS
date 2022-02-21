0f4536f7407d67f092b993f1b4397f481a11aeb4
Young Zoo Park <enkeejuniour@naver.com>
2022-02-21 15:18:36 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const title = document.querySelector(".title h1");
+
+function handleTitleClick(){
+    console.log("Title was clicked!");
+}
+
+title.style.color = "blue";
+title.addEventListener("click", handleTitleClick);
+
 console.log(title);

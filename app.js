08bd8b755699fa4af49f308b1b42b25d8493bc4a
Young Zoo Park <enkeejuniour@naver.com>
2022-02-22 13:07:37 +0900

    js commit

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+function onLoginBtnClick(){
+    const username = loginInput.value;
+    if (username == ""){
+        alert("No input value");
+    }
+    else if (username.length > 15){
+        alert("Your name is too long")
+    }
+    else {
+        console.log("click!");
+        alert("hi "+username);
+    }
+}
+loginButton.addEventListener("click",onLoginBtnClick);

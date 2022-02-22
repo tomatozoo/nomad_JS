2256bd95f9a49e384415150d0c23ed9c294425c3
Young Zoo Park <enkeejuniour@naver.com>
2022-02-22 15:10:17 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -3,13 +3,11 @@ const clock = document.querySelector("#clock");
 // interval
 function sayHello(){
     const date = new Date();
-    clock.innerText = `${date.getHours()} : ${date.getMinutes()}`;
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
+sayHello();
 setInterval(sayHello, 1);
-// timeout
-//setTimeout(sayHello, 5000);
-
-const date = new Date();
-date.getDate();
-date.getMonth();
-date.getFullYear();
